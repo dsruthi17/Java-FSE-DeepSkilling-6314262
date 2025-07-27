570f620b247af9be3ce66e2ac9380442addde903
dsruthi17 <damarlasruthisri@gmail.com>
2025-07-27 14:57:29 +0530

    initial commit

---
+++ myfirstreact/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div>Welcome the first session of React</div>;
+}
+
+export default App;
---
+++ studentapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
